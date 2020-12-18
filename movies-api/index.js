d1189d136de491019f75f2ff8ce82816321d7cf2
Ziqi Zhou <20091593@mail.wit.ie>
2020-12-18 13:07:01 +0000

    Week 9 Included Mongoose

--- movies-api/index.js
+++ movies-api/index.js
@@ -2,6 +2,9 @@ import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
 import bodyParser from 'body-parser';
+import './db';
+import {loadUsers} from './seedData';
+
 
 dotenv.config();
 
@@ -13,6 +16,10 @@ const errHandler = (err, req, res, next) => {
   }
   res.status(500).send(`Hey!! You caught the error 👍👍, ${err.stack} `);
 };
+
+if (process.env.SEED_DB) {
+  loadUsers();
+}
 const app = express();
 
 const port = process.env.PORT;
